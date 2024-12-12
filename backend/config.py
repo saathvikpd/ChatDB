eef58245ed8e21d97a27b2893f7324acd2b3ddb3
saathvikpd <saathvikpd@users.noreply.github.com>
2024-12-11 23:12:10 -0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,3 @@
-# Database Configuration
-DB_CONFIG = {
-    'user': 'your_username',
-    'password': 'your_password',
-    'host': 'localhost',
-    'database': 'your_database',
-}
-
 # XAI API Key
 XAI_API_KEY = 'xai-IWDeHQuxQqHpju5nrFe7yIwqYn73zeAMSsV3IgSczNem4b4YfD2CWTZd5AlqQorcmcsUw0vBUYEa49lz'
 
