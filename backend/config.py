b2e3c230937afcc8664567f8e1bee60a56f05d56
saathvikpd <saathvikpd@users.noreply.github.com>
2024-12-11 23:11:54 -0800

    Update config.py

--- backend/config.py
+++ backend/config.py
@@ -9,5 +9,5 @@ DB_CONFIG = {
 # XAI API Key
 XAI_API_KEY = 'xai-IWDeHQuxQqHpju5nrFe7yIwqYn73zeAMSsV3IgSczNem4b4YfD2CWTZd5AlqQorcmcsUw0vBUYEa49lz'
 
-# Anchoring max retry count
+# Anchoring Max Retry Count
 ANCHOR_TOL = 5
